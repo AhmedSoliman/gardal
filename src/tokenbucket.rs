efab8139c638af2c4f48ca29a6cf4998594cb4ed
Ahmed Farghal <me@asoli.dev>
2025-07-14 20:10:26 +0100

    remove unused file

--- src/tokenbucket.rs
+++
@@ -1,8 +0,0 @@
-use crate::{RateLimited, Tokens};
-
-pub trait TokenBucket {
-    fn consume(&self, to_consume: f64) -> Option<Tokens>;
-    fn try_consume(&self, to_consume: f64) -> Result<Tokens, RateLimited>;
-    fn saturating_consume(&self, to_consume: f64) -> Option<Tokens>;
-    fn add_tokens(&self, tokens: impl Into<f64>);
-}
